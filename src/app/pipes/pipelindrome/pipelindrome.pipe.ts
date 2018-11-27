53ba063bd83c021fedc9955d417aba5a2a04882f
docbrownaa <gduvinage@gmail.com>
2018-11-27 16:42:52 +0100

    utilisation du reverse

--- src/app/pipes/pipelindrome/pipelindrome.pipe.ts
+++ src/app/pipes/pipelindrome/pipelindrome.pipe.ts
@@ -6,12 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PipelindromePipe implements PipeTransform {
 
   transform(value: string ): string {
-    const transform = value.split('');
-    const ret = [];
-    for (let i = transform.length; i >= 0; i--) {
-      ret.push(transform[i]);
-    }
-    return ret.join('');
+   return value.split('').reverse().join('');
   }
 
 }
